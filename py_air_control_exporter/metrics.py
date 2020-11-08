9c771b4dfd369abf3da22645334aee8c583ca51e
Matej Urbas <matej.urbas@gmail.com>
2020-11-08 12:41:31 +0000

    migrates to the py-air-control library
    This change moves away from the discontinued `python-air-purifier` library in favour of the `py-air-control` library.

---
+++ py_air_control_exporter/metrics.py
@@ -0,0 +1,59 @@
+import logging
+from os import environ
+
+from flask import Blueprint, abort
+from pyairctrl import coap_client, http_air_client, plain_coap_client
+
+from py_air_control_exporter import prometheus
+
+API = Blueprint("metrics", __name__, url_prefix="/metrics")
+HOST_ENV_VAR = "PY_AIR_CONTROL_HOST"
+PROTOCOL_ENV_VAR = "PY_AIR_CONTROL_PROTOCOL"
+HTTP_PROTOCOL = "http"
+COAP_PROTOCOL = "coap"
+PLAIN_COAP_PROTOCOL = "plain_coap"
+
+
+@API.route("")
+def get():
+    try:
+        host = environ[HOST_ENV_VAR]
+    except KeyError:
+        logging.error(
+            "Please specify the host address of the air control device via the environment variable %s",
+            HOST_ENV_VAR,
+        )
+        return abort(500)
+    client = _get_client(host)
+    if client is None:
+        return abort(500)
+    try:
+        status = client.get_status()
+        logging.debug("Got the following status from py-air-control: %s", status)
+        return prometheus.to_metrics(status)
+    except Exception as ex:
+        logging.error(
+            "Could not read values from air control device %s. Error: %s",
+            host,
+            ex,
+        )
+        return abort(501)
+
+
+def _get_client(host):
+    protocol = environ.get(PROTOCOL_ENV_VAR, HTTP_PROTOCOL)
+    if protocol == HTTP_PROTOCOL:
+        return http_air_client.HTTPAirClient(host)
+    if protocol == COAP_PROTOCOL:
+        return coap_client.CoAPAirClient(host)
+    if protocol == PLAIN_COAP_PROTOCOL:
+        return plain_coap_client.PlainCoAPAirClient(host)
+    logging.error(
+        "Unknown protocol '%s'. Please set the environment variable '%s' to one of the following: %s, %s, %s",
+        protocol,
+        PROTOCOL_ENV_VAR,
+        HTTP_PROTOCOL,
+        COAP_PROTOCOL,
+        PLAIN_COAP_PROTOCOL,
+    )
+    return None
