850e7b44c41ad59141f94433cba70b1f19701c60
Matej Urbas <matej.urbas@gmail.com>
2020-12-05 16:13:48 +0000

    adds license and url to a simplified setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@
 
 """The setup script."""
 
-import packaging.version
-import setuptools_scm
 from pathlib import Path
 from setuptools import setup
 
@@ -13,7 +11,7 @@ REQUIREMENTS = [
     "py-air-control>=2.0.0",
 ]
 
-SETUP_REQUIREMENTS = ["pytest-runner", "setuptools_scm"]
+SETUP_REQUIREMENTS = ["pytest-runner"]
 
 TEST_REQUIREMENTS = ["pytest"]
 
@@ -21,31 +19,13 @@ CHANGELOG = Path("CHANGELOG.md").read_text()
 
 long_description = f"{Path('README.md').read_text()}\n\n{CHANGELOG}"
 
-
-def get_version():
-    try:
-        version = setuptools_scm.get_version()
-        changelog_expected_version = ".".join(
-            str(el) for el in packaging.version.parse(version).release
-        )
-        changelog_version = CHANGELOG.splitlines()[0].strip("#").strip()
-        assert changelog_version >= changelog_expected_version, (
-            f"Unexpected version in CHANGELOG.md's top section. Should be greater "
-            f"than or equal to {changelog_expected_version} but is "
-            f"{changelog_version}. Please add a section for the upcoming release."
-        )
-        return version
-    except LookupError:
-        # NB: we could be building from a tarball.
-        return "0.0.0"
-
-
 setup(
     author_email="matej.urbas@gmail.com",
     author="Matej Urbas",
     include_package_data=True,
     install_requires=REQUIREMENTS,
     keywords=["py-air-control-exporter", "py-air-control", "prometheus", "exporter"],
+    license="MIT license",
     long_description_content_type="text/markdown",
     long_description=long_description,
     name="py-air-control-exporter",
@@ -53,5 +33,6 @@ setup(
     setup_requires=SETUP_REQUIREMENTS,
     test_suite="test",
     tests_require=TEST_REQUIREMENTS,
-    version=get_version(),
+    url="https://github.com/urbas/sgp30-exporter",
+    version="0.1.5",
 )
